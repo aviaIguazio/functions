eec0677cb38ccee8ef553f2a37ad8172dfb38522
Avi Asulin <avia@iguazio.com>
2021-04-20 17:43:55 +0300

    separate code from notebook

--- sentiment_analysis_serving/sentiment_analysis.py
+++ sentiment_analysis_serving/sentiment_analysis.py
@@ -16,6 +16,12 @@ class BertSentimentClassifier(nn.Module):
         self.softmax = nn.Softmax(dim=1)
 
     def forward(self, input_ids, attention_mask):
+        """
+
+        :param input_ids:
+        :param attention_mask:
+        :return:
+        """
         _, pooled_out = self.bert(
             input_ids=input_ids,
             attention_mask=attention_mask
@@ -30,6 +36,11 @@ class SentimentClassifierServing(mlrun.serving.V2ModelServer):
         self.model = self.load()
 
     def load(self):
+        """
+        load bert model into class
+
+        :return: model
+        """
         model_file, _ = self.get_model('.pt')
         device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
         model = BertSentimentClassifier(n_classes=3)
@@ -38,6 +49,12 @@ class SentimentClassifierServing(mlrun.serving.V2ModelServer):
         return model
 
     def predict(self, body):
+        """
+        predict function
+
+        :param body: set of inputs for bert model to predict from
+        :return: list of sentiment predictions
+        """
         try:
             instances = body['inputs']
             enc = tokenizer.batch_encode_plus(instances, return_tensors='pt', pad_to_max_length=True)
