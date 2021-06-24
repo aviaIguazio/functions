7993979c96259d945072618756b863e1a76918c5
Avi Asulin <avia@iguazio.com>
2021-06-24 09:39:28 +0300

    add spec to code_to_function test to allow debugging

--- sklearn_classifier/test_sklearn_classifier.py
+++ sklearn_classifier/test_sklearn_classifier.py
@@ -1,8 +1,9 @@
-from mlrun import import_function
+from mlrun import import_function, code_to_function
 from pathlib import Path
 
 DATA_URL = 'https://s3.wasabisys.com/iguazio/data/iris/iris_dataset.csv'
 CLASSIFIER_PATH = "artifacts/default/latest/arc-to-parquet-arc_to_parquet_price.yaml"
+
 
 def generate_data():
     data_url = "https://raw.githubusercontent.com/parrt/random-forest-importances/master/notebooks/data/rent.csv"
@@ -31,3 +32,24 @@ def test_import_sklearn_classifier():
     assert Path(CLASSIFIER_PATH).is_file()
 
 
+def test_code_to_function_sklearn_classifier():
+    acquire_run = generate_data()
+    fn = code_to_function(name='test_sklearn',
+                          filename="sklearn_classifier.py",
+                          handler="train_model",
+                          kind="local",
+                          )
+    # define model
+    params = {
+            "sample": -5_000,  # 5k random rows,
+            "model_pkg_class": "sklearn.ensemble.RandomForestClassifier",
+            "label_column": "interest_level",
+            "CLASS_n_estimators": 100,
+            "CLASS_min_samples_leaf": 1,
+            "CLASS_n_jobs": -1,
+            "CLASS_oob_score": True}
+    fn.spec.command = "sklearn_classifier.py"
+    train_run = fn.run(params=params, inputs={"dataset": acquire_run.outputs["price"]}
+                       #,local=True,
+                       ,artifact_path="artifacts")
+    assert Path(CLASSIFIER_PATH).is_file()
